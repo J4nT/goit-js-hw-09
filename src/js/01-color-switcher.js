3414f0ee95172f9f4d0f51091e90e936e2b3fe1d
J4nT <145610618+J4nT@users.noreply.github.com>
2024-02-21 21:32:26 +0100

    task01+tree

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const webBackground = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let intervalId = null;
+
+buttonStart.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+        webBackground.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+});
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(intervalId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+});
